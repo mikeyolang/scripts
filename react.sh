ce3b90385ebf0f47e24532ce812355860815b2e6
Orinda Felix Ochieng <55891685+forinda@users.noreply.github.com>
2022-03-14 19:09:02 +0300

    Update react.sh

--- react.sh
+++ react.sh
@@ -1,1 +1,21 @@
-
+#!/usr/bin/bash
+figlet "< React app >"
+echo Enter app name you wish to create:
+read appName
+if [ -z "$appName" ]; then
+    echo "No app name provided exiting...."
+    sleep 3
+    exit
+else
+    echo "Creating your app $appName"
+    yarn create react-app $appName
+    cd $appName
+    yarn add redux axios @reduxjs/toolkit react-router-dom react-redux sass
+    yarn add @mui/icons-material @mui/material @reduxjs/toolkit styled-components
+    yarn add redux-persist @emotion/styled @emotion/react @emotion/core
+    yarn add moment moment-timezone
+    echo Opening VS-code.....
+    sleep 5
+    code .
+    
+fi
